11eda286d9b189a32e812a5b31e1de09aed6b9fa
Mohamed-Aziz-Mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2021-07-16 21:12:08 +0100

    modif

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -8,9 +8,6 @@ export default class Snake {
         this.body.push(
             this.scene.add.rectangle(0,0,16,16,0xff0000).setOrigin(0)
         );
-        this.body.push(
-            this.scene.add.rectangle(0,0,16,16,0x0000ff).setOrigin(0)
-        );
         scene.input.keyboard.on('keydown' , e => {
             this.keydown(e);
         });
@@ -42,9 +39,12 @@ export default class Snake {
     }
 
     move() {
-        this.body[1].x = this.body[0].x;
-        this.body[1].y = this.body[0].y;
+        for (let index = this.body.length - 1; index>0; index--) {
+            this.body[index].y = this.body[index - 1].y;
+            this.body[index].x = this.body[index - 1].x;
+        }
         this.body[0].x += this.direction.x * 16;
         this.body[0].y += this.direction.y * 16;
+       
     }
 }

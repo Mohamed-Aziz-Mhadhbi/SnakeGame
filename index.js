0e65fd16ec26339455cdd0e1d8ab7f942c7923b5
Mohamed-Aziz-Mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2021-07-16 20:02:16 +0100

    modificatin

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static(__dirname + '/public/'));
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html')
+  res.sendFile(__dirname + 'index.html')
 })
 
 app.listen(port, () => {

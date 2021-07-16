e60bf2cff569f012fd762dd0b677cf1fa9a2055c
Mohamed-Aziz-Mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2021-07-16 20:23:08 +0100

    changing the box to table(body)

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -1,10 +1,13 @@
 export default class Snake {
     constructor(scene){
         this.scene = scene;
-        this.box = this.scene.add.rectangle(0,0,16,16,0xff0000).setOrigin(0);
+        this.body = [];
+        this.body.push(
+            this.scene.add.rectangle(0,0,16,16,0xff0000).setOrigin(0)
+        );
     }
 
     update(time) {
-        this.box.x +=1;
+        this.body[0].x +=1;
     }
 }
